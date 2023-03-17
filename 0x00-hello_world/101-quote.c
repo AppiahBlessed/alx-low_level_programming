025b69bd85636d301c82c8ec93dc01d11f30b076
Dicey <kwabenadicey@gmail.com>
2023-03-17 05:18:53 -0700

    Print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-#include <unist101-quote.cd>
-/**
- * main - main entry
- *
- * Return: Always (0) success
- */
+#include <unistd.h>
 
+/**
+ * main - write to stdout
+ * Return: Always 0
+ */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
