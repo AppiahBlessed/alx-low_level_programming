9526627e73a2e0f5197c7454e9be083081a4746c
Dicey <kwabenadicey@gmail.com>
2023-03-17 16:43:57 -0700

    File content correctd

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,8 @@
-nes (16 sloc)  249 Bytes
 
 #include <stdio.h>
 
 /**
  * main - prints the lowercase alphabet in reverse,
- * followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
