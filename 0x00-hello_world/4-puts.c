175c7552edf46886ac0ada33615b6fa20c4e3554
Dicey <kwabenadicey@gmail.com>
2023-03-17 03:24:49 -0700

    Using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int 4 - puts.c(void)
+/**
+ * main-main block
+ * Return: 0
+ */
+int main(void)
 {
-	puts('"Programming is like building a multilingual puzzle');
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
