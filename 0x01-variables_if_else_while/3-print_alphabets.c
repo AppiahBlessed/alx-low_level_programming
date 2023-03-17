aae012aa5cb6b983fd73458245939da40f0578b7
Dicey <kwabenadicey@gmail.com>
2023-03-17 15:51:44 -0700

    Print Upper case and lower case alph..

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while
+		(ch <= 'z');
+	{
+		putchar(ch);
+		ch++;
+	}
+
+	ch = 'A';
+	while
+		(ch <= 'Z');
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
