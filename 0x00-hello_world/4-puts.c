9d788180c73f8c9367567858e2457f2fef2429b1
Dicey <kwabenadicey@gmail.com>
2023-03-17 03:14:10 -0700

    Usin puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-int main(void)
+int 4 - puts.c(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts('"Programming is like building a multilingual puzzle');
 
 	return (0);
 }
