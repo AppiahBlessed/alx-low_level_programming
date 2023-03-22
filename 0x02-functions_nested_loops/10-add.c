01dd99a6c340717a03899ee00403ba9ebefe2c1c
Dicey <kwabenadicey@gmail.com>
2023-03-22 16:12:44 -0700

    Add two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add -Adds to numbers
+ *@a: Variable placeholder
+ *@b: variable placeholder
+ *Return: 0 for successful
+ */
+
+int add(int a, int b)
+{
+	int i;
+
+	i = a + b;
+	return (i);
+}
