13e29818bbc92d5e1f45f3b5268de389b9408e58
Dicey <kwabenadicey@gmail.com>
2023-03-22 13:24:19 -0700

    Absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - prints for absolute value
+ * Return: True or False
+ * @int: Data type
+ *
+ */
+
+int _abs(int a)
+{
+
+	if (a < 0)
+	{
+		a = -(a);
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
