146a7d2bc7cc58434c130d4d30fd93cd698c3f1d
Dicey <kwabenadicey@gmail.com>
2023-03-22 16:53:17 -0700

    Print numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,18 @@ void print_to_98(int n)
 {
 	for (n = n; n <= 98; n++)
 	{
+		for (n = n; n > 98; n-- )
+			printf("%d", n);
 		if (n != 98)
 		{
 		printf("%d", n);
 		printf(", ");
 		}
-		else 
-		{
+	
+			else 
+			{
 			printf("%d", n);
-		}
+			}
 	}
-
-	printf("\n");
-
+		printf("\n");
 }
