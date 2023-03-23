9e9577efa6a3ef80ed9e22c0dd388ab0d00f9241
Dicey <kwabenadicey@gmail.com>
2023-03-22 17:25:51 -0700

    Printnatural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,34 @@
 
 void print_to_98(int n)
 {
-	for (n = n; n <= 98; n++)
+	if (n >= 0 && n <= 98)
 	{
-		for (n = n; n > 98; n-- )
-			printf("%d", n);
-		if (n != 98)
+		for (n = n; n <= 98; n++)
 		{
-		printf("%d", n);
-		printf(", ");
-		}
-	
-			else 
-			{
 			printf("%d", n);
-			}
+			printf(", ")
+		}
+			printf("\n")
+	}
+	else if (n < 0)
+
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d", n);
+			printf(", ");
+		}
+			printf("\n")
+	}
+	else if (n > 0)
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d", n);
+		}
 	}
 		printf("\n");
+	else if (n == 98)
+		printf("%d", n)
+		printf(", ");
 }
