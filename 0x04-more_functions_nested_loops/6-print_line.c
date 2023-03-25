8e2c1f13e1b66a23ea067a65342bf6d6303cc9e3
Dicey <kwabenadcey@gmail.com>
2023-03-25 00:55:04 +0000

    Print dashed lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_line - Prints line
+ *@n: variable
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1; i <= n; ++i)
+{
+	_putchar('_');
+
+}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	_putchar('\n');
+
+}
