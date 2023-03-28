fdf3e51a0041a3314447a1fa3f44c69088760c21
Dicey <kwabenadcey@gmail.com>
2023-03-28 15:24:32 +0000

    Splits string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,7 +15,7 @@ void puts_half(char *str)
 		for (i = len / 2; str[i] != '\0'; i++)
 		_putchar(str[i]);
 		}
-	else
+	else if (len % 2 != 0)
 	{
 		for (i = (len - 1) / 2 ; str[i] != '\0'; i++)
 			_putchar(str[i]);
