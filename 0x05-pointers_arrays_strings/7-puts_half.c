f0027f56ce6165aa9eaae65d3d3c5d58acda2a3f
Dicey <kwabenadcey@gmail.com>
2023-03-28 15:20:17 +0000

    Splits string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *puts_half - Function that splits string
+ *@str: String variable
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	if (len % 2 == 0)
+		{
+		for (i = len / 2; str[i] != '\0'; i++)
+		_putchar(str[i]);
+		}
+	else
+	{
+		for (i = (len - 1) / 2 ; str[i] != '\0'; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
