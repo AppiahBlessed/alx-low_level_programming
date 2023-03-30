7f51a27c4d4164131e2db3876ec83c72f0cb643e
Dicey <kwabenadcey@gmail.com>
2023-03-30 18:30:37 +0000

    Rot-13 encoding

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- *
- *
- *
+ *rot13 - Rot-13 encoding
+ *@str: String value
+ *Return: Encoded string
  */
 char *rot13(char *str)
 {
 	int i;
 	int j;
-	char regular[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";/* declare array to hold regular alphabet string*/
-	char encoded[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";/* declare array to hold rot-13 replica string*/
+	char regular[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char encoded[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++) /* Iterate over the regular set*/
 		{
-			if (str[i] == regular[j]) /* Check for similarkities and replace with rot-13*/
+			if (str[i] == regular[j])
 			{
 				str[i] = encoded[j];
 				break;
