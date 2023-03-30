6c469d61d8e58b7f6f788e20fb6e3b5afcb1e544
Dicey <kwabenadcey@gmail.com>
2023-03-30 15:48:14 +0000

    Capitalize string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- *
- *
- *
+ *string_toupper - Change to upper case
+ *@tr: Pointer to string
+ *Return: Returns upper case string
  */
 char *string_toupper(char *tr)
 {
 	int i;
 
-	for (i = 0; tr[i] != '\0'; i++ )
+	i = 0;
+	while (tr[i] != '\0')
 	{
 		if (tr[i] >= 'a' && tr[i] <= 'z')
 			tr[i] = tr[i] - 32;
-
+		i++;
 	}
 	return (tr);
 }
