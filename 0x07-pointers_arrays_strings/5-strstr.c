e0b74ee7cd6f52ed1467fb963d92750778c7f4fe
Dicey <kwabenadcey@gmail.com>
2023-04-03 23:31:43 +0000

    Run man strstr to learn more

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,15 +11,14 @@ char *_strstr(char *haystack, char *needle)
 	for (; *haystack != '\0'; haystack++)
 	{
 		char *first = haystack;
-        	char *second = needle;
-	
+		char *second = needle;
+
 		while (*first == *second && *second != '\0')
 		{
 			first++;
 			second++;
 		}
-	
-		if (*second == '\0')	
+		if (*second == '\0')
 			return (haystack);
 	}
 	return (NULL);
