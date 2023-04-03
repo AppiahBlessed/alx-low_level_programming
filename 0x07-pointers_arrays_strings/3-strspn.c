fff7fede8a1fc8bd7fe29dc279c8c0bb7429ea68
Dicey <kwabenadcey@gmail.com>
2023-04-03 22:46:16 +0000

    Run man strspn to learn more.

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *_strspn - Function man strspn
+ *@s: String
+ *@accept - Accept
+ *Return: Returns int
+ *
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int num;
+	int i;
+	num = 0;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			
+			if (accept[i] == *s)
+			{
+					num++;
+					break;
+			}
+			else if ((accept[i + 1]) == '\0')
+			{
+			return (num);
+			}
+		}
+		s++;
+	}
+	return (num);
+}
