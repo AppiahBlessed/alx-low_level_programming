54eacc378fb6f1ba81c526e4bfa0b2d0ecfdb401
Dicey <kwabenadcey@gmail.com>
2023-04-11 16:27:01 +0000

    Using Malloc()

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,15 +13,18 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = malloc(size * sizeof(char));
+	if (s == NULL)
+	{
+		return(NULL);
+	}
 	if (size == 0)
 	{
 		return (NULL);
-		return (s);
 	}
 	for (i = 0; i <= size; i++)
 	{
 		s[i] = c;
 	}
 	return (s);
-	return (NULL);
+
 }
