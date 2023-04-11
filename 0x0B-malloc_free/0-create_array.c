98e56af857f55b54b099da371cc9d64eb370c16c
Dicey <kwabenadcey@gmail.com>
2023-04-11 15:50:38 +0000

    Using Malloc()

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *
- *
- *
- *
+ *create_array - Creates an array of strings containing c
+ *@size: Determkines the size of the array
+ *@c: Character containing string
+ *Return: Returns pointer to string
  *
  */
 char *create_array(unsigned int size, char c)
@@ -13,8 +13,8 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	s = malloc(size * sizeof(char));
-	 if (size == 0)
-		return NULL;
+	if (size == 0)
+		return (NULL);
 	for (i = 0; i <= size; i++)
 		s[i] = c;
 	return (s);
