fb93dabdb18c393dd88ca346f0b162afd3db98c3
Dicey <kwabenadcey@gmail.com>
2023-04-13 13:17:32 +0000

    Add integer arguements

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main- Function computes integer arguements
+ *@argc: Number of allowed arguements
+ *@argv: Pointer to arguements array
+ *Return: Returns 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, result;
+
+	if (argc != 3)/*Checkk for number of arguements*/
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);/*Using atoi to convert char to int*/
+	b = atoi(argv[2]);
+	result = a + b;/*Add integers*/
+	printf("%d\n", result);/*Print result with new line*/
+	return (0);
+}
