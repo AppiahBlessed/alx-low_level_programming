21d4ed19014c904c473aaaa4786f1888b3929ad3
Dicey <kwabenadcey@gmail.com>
2023-04-13 10:22:43 +0000

    Betty Documentation

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
- *
- *
- *
+ *main- Function passed arguments argc and argv
+ *@argc: Number of arguements
+ *@argv: Array of content of arguement
+ *Return: Returns 0 on success
  */
 int main(int argc, char *argv[])
-{	if (argc == 1)
+{
+	if (argc == 1)
 	{
 		printf("%s", argv[0]);
 		printf("\n");
