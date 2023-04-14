df3ac9d761ff369643ba127cd2e8c7dd9a010f7a
Dicey <kwabenadcey@gmail.com>
2023-04-14 08:10:53 +0000

    Compute cummulative arguements

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - Adds arguements
+ *@argc: Number of expected arguements
+ *@argv: Array of arguements
+ *Return: Returns 1 on success
+ */
+int main(int argc, char *argv[])
+{
+	int b, a, result;
+
+	if (argc < 1)/*Condition for if only one arguements is passed*/
+	{
+		printf("0");
+	}
+	else
+	{
+		for (a = 1; a < argc; a++)
+		{
+			b = atoi(argv[a]);/*Convert array string to integer and assign to b*/
+			if (b > 0)
+			{
+				result = result + b;/*Cumulative*/
+			}
+			else/*Create condition to print error is arguement is not int*/
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+	}
+	printf("%d\n", result);/*Print results*/
+	return (1);
+
+}
+
