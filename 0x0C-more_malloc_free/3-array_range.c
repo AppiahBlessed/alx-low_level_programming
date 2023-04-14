83e1c01c59e51194b57e1e9b7d53f5059028aaf1
Dicey <kwabenadcey@gmail.com>
2023-04-14 22:55:15 +0000

    Print range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,7 +12,7 @@ int *array_range(int min, int max)
 	int i;
 	int *dest;
 	/*Plus one to handle null byte terminator*/
-	dest = malloc((max - min +1) * sizeof(int));
+	dest = malloc((max - min + 1) * sizeof(int));
 	if (dest == NULL)/*Return if malloc fails*/
 	{
 		return (NULL);
@@ -25,5 +25,5 @@ int *array_range(int min, int max)
 	{/*Assign min increasing by one to dest in all index*/
 		dest[i] = min + i;
 	}
-	return (dest);/**Return pointer to array/
+	return (dest);/*Return pointer to array*/
 }
