301bf984256dce9d8f9abf3af009c56145a8d26c
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-17 17:59:41 +0000

    Requirement correction

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  *main - Print current file name
- *Return: Returns 1 on success
+ *Return: Returns 0 on success
  *
  */
 int main(void)
 {
-	printf("%s\n", __FILE__);
-	return (1);
-
+	printf("%s\n", __FILE__);/*Macro that prints filename*/
+	return (0);
 }
