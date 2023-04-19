2a0e6c9f583ebce5b331bd897d964057aec94dc0
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-19 11:53:08 +0000

    NULL

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if ((*d).name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	if ((*d).owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+	if (d == NULL)
+	{
+		printf(" ");
+	}
+		else
+		{
+			printf("Name: %s\n", (*d).name);
+			printf("Age: %f\n", (*d).age);	
+			printf("Owner: %s\n", (*d).owner);
+		}
+}
