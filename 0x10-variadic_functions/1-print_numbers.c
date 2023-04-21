ab4c30b83973f301e593bf29ee45d7216ac29f77
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-21 04:21:56 +0000

    Betty reulations

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- *
- *
- *
+ *print_numbers- Prints numbers with a seperation charater
+ *@separator: Seperator char
+ *@n: Expected number of arguements
  *
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -16,16 +16,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	/*Loop to get elements or arguements*/
 	for (i = 0; i < n; i++)
-	{
+	{/*Print nothing if character is null*/
 		if (separator == NULL)
 		{
-		 printf(" ");
+			printf(" ");
 		}
 			else
 			{
-				if (i != n-1)
+				if (i != n - 1)/*Exclude seperator from last element*/
 				{
-				printf("%d%s", va_arg(list, int), separator);
+					printf("%d%s", va_arg(list, int), separator);
 				}
 					else
 					{
