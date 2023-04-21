ba24c58c58aa79b9804ba442e38aa4ea1341fa2b
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-21 04:37:38 +0000

    Handle Null

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,22 +16,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_start(list, n);
 	/*Loop to get elements or arguements*/
 	for (i = 0; i < n; i++)
-	{/*Print nothing if character is null*/
-		if (separator == NULL)
+	{
+		printf("%d%s", va_arg(list, int), separator);
+		if (i == n-1 && separator != NULL)
 		{
-			printf(" ");
+			printf("%d\n", va_arg(list, int));
 		}
-			else
-			{
-				if (i != n - 1)/*Exclude seperator from last element*/
-				{
-					printf("%d%s", va_arg(list, int), separator);
-				}
-					else
-					{
-						printf("%d\n", va_arg(list, int));
-					}
-			}
 
 	}
 	va_end(list);/*End macro*/
