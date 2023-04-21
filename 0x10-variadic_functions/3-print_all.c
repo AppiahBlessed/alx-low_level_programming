83f6e5f33a4852b17605efe6536d1d93e8a6d4a0
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-21 05:53:43 +0000

    File

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ *print_all - Function prints all data types
+ *@format: Format of value to print
+ *
+ *
+ */
+void print_all(const char * const format, ...)
+{
