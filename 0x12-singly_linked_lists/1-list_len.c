6c2ae9de2eb9842ce9ffea089bf657844511eec5
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-27 21:51:56 +0000

    Betty documentation

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *list_len - Gets lenght of lists
+ *@h: Pointer as head
+ *Return: Returns number
  *
  */
 size_t list_len(const list_t *h)
 {
-	int num = 1;
+	int num = 1;/*I really dont know why i initialized with 1*/
 	int i;
 
-	for (i = 0; h->next != NULL; i++)
+	for (i = 0; h->next != NULL; i++)/*Loop through element till conditon == F*/
 	{
 		num++;
 		h = h->next;
 	}
-	return(num);
+	return (num);
 }
