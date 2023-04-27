e0c1b973790c54ada2182d8384ff2ffb7fbb4023
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-27 21:55:49 +0000

    Hot fix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,10 +10,13 @@ size_t list_len(const list_t *h)
 	int num = 1;/*I really dont know why i initialized with 1*/
 	int i;
 
-	for (i = 0; h->next != NULL; i++)/*Loop through element till conditon == F*/
-	{
-		num++;
-		h = h->next;
-	}
+	if (h->next != NULL)
+		for (i = 0; h->next != NULL; i++)/*Loop through element till conditon == F*/
+		{
+			num++;
+			h = h->next;
+		}
+	else
+		num = 0;
 	return (num);
 }
