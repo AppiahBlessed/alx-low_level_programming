d96a6b6a1b032125b22d1a73dbc21c1c3dcf0320
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-30 11:06:43 +0000

    Hot fix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,16 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
-	listint_t *next;/* Pointer to the next node*/
-
-	temp = *head;/*Pointer to the current node*/
 
 	if (head == NULL)
 	{
 		return;
 	}
-	while (temp != NULL)
+	while (*head)
 	{
-		next = temp->next;/*Store a pointer to the next node*/
-		free(temp);/*free current node*/
-		temp = next;/*Enter next node*/
+		temp = (*head)->next;/*Store a pointer to the next node*/
+		free(*head);/*free current node*/
+		*head = temp;/*Enter next node*/
 	}
 	*head = NULL;/*Set the original pointer to NULL*/
 
