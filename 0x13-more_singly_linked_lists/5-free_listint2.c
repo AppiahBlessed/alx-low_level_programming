0bccd9a771384fd41e5d671db73bde88daaede91
AppiahBlessed <kwabenadcey@gmail.com>
2023-04-30 10:59:53 +0000

    Hot firx

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,7 +10,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp = *head;/*Pointer to the current node*/
 	listint_t *next;/* Pointer to the next node*/
-
+	
+	if (head == NULL)
+	{
+		return;
+	}
 	while (temp != NULL)
 	{
 		next = temp->next;/*Store a pointer to the next node*/
