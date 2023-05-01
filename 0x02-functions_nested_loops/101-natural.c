125f34e8510434e31e5582252f31b1547ba63e03
AppiahBlessed <kwabenadcey@gmail.com>
2023-05-01 19:06:29 +0000

    Hot fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,16 +6,13 @@
  */
 int main(void)
 {
-	int i;
-	int sum;
+		int i, sum = 0;
 
-	sum = 0;
-	for (i = 3; i < 1024; i++)
-		if ((i % 3 == 0) || (i % 5 == 0))
+		for (i = 0; i < 1024; i++)
 		{
-			sum = i + sum;
-			printf("\n");
-			printf("%d", sum);
+			if ((i % 3) == 0 || (i % 5) == 0)
+				sum += i;
 		}
-	return (0);
+		printf("%d\n", sum);
+		return (0);
 }
