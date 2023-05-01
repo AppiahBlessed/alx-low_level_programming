638445fbec03ac53e5873ebf7836365383758b91
AppiahBlessed <kwabenadcey@gmail.com>
2023-05-01 20:15:29 +0000

    Re-code

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,9 +1,24 @@
 #include "main.h"
 /**
-*print_rev - Prints string in reverse
+*rev_string - Prints string in reverse
 *@s: Variable pointer
 */
 void rev_string(char *s)
 {
+	int num = 0;
+	int i;
+
+	while (*s != '\0')
+	{
+		num++;
+		s++;
+	}
+	s--;
+	for (i = num; i > 0; i--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
 
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
- *
- *
+ *rev_string - Reverses string
+ *@s: String pointer
  */
 void rev_string(char *s)
 {
+	char rev = s[0];
+	int count = 0;
 	int i;
-	int len = 0;
-	char j;
 
-	for (i = 0; s[i] != '\0'; i++)
-		len++;
-	for (i = len; i >= 0; i--)
+	while (s[count] != '\0')
+	count++;
+	for (i = 0; i < count; i++)
 	{
-		j = s[i];
-			
+		count--;
+		rev = s[i];
+		s[i] = s[count];
+		s[count] = rev;
 	}
+
 }
