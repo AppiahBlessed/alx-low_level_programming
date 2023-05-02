eb030ea2083dfff3bfa800a3073732e8dd3a0fa0
AppiahBlessed <kwabenadcey@gmail.com>
2023-05-02 06:52:55 +0000

    Fixed bug

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s;
-	unsigned int i = 0
+	unsigned int i = 0;
 	unsigned int j = 0;
 	unsigned int len1 = 0;
 	unsigned int len2 = 0;
