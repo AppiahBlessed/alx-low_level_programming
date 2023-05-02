3bb069bc3d4cecd7dec355719270b623019baeb6
AppiahBlessed <kwabenadcey@gmail.com>
2023-05-02 04:28:20 +0000

    betty requirement

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,8 +9,9 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int id;
+
 	if (array == NULL || size <= 0 || cmp == NULL)
-		 return (-1);
+		return (-1);
 
 	for (id = 0; id < size; id++)
 	{
