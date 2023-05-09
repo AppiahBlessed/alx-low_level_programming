2dafbdb105ce1ae7e5ce7f930874acf8bc0cda27
AppiahBlessed <kwabenadcey@gmail.com>
2023-05-09 16:08:44 +0000

    Hot fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,7 +18,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (count = 0; text_content[count])
+		for (count = 0; text_content[count];)
 		{
 			count++;/*Length of text_content*/
 		}
