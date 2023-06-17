00c1da5f0cae8a1cc65fb4cc407b7b3a71d55aea
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-17 16:37:09 +0000

    Met betty requirement

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -5,13 +7,13 @@
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
- * 
+ *
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 #include <stdio.h>
 #include <stdlib.h>
@@ -19,3 +21,4 @@ size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+#endif
