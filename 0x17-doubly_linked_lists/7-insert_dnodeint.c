121ce2a2a0cce979aef06b989882e27034887d00
AppiahBlessd <kwabenadicey@gmail.com>
2023-06-17 19:47:37 +0000

    Add node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,7 +8,7 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode, *temp, *next_node;
+	dlistint_t *newnode, *temp;
 	unsigned int i = 1;
 
 	temp = *h;
@@ -17,22 +17,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	else if (idx == 0)
+	if (idx == 0)
 	{
 		add_dnodeint(h, n);/*Called from file 2*/
 	}
-	else
+	while (temp != NULL && i < idx)/*Fall at exact index*/
 	{
-		newnode->n = n;
-		while (i < idx)
-		{
-			temp = temp->next;
-			i++;
-		}
-		next_node = temp->next;
-		newnode->prev = temp;
-		newnode->next = next_node;
-		temp->next = newnode;
+		temp = temp->next;
+		i++;
 	}
+	if (temp == NULL)/*When impossible to insert at position*/
+	{
+		return (NULL);
+	}
+	newnode->n = n;
+	newnode->prev = temp;
+	newnode->next = temp->next;
+	if (temp->next != NULL)
+	{
+		temp->next->prev = newnode;
+	}
+	temp->next = newnode;
 	return (newnode);
 }
