2a6eb33f3a595fc74e29cfad9de63e5df6e2af84
AppiahBlessd <kwabenadicey@gmail.com>
2023-07-07 02:28:29 +0000

    Search for value using key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ *hash_table_get - Search and retrieve value
+ *@ht: HashTable
+ *@key: Specific key value
+ *Return: Returns found value or Null if
+ *it doesnt exist
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current;
+/*Handle errors*/
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+/*Get indexx using key*/
+	index = key_index((const unsigned char *)key, ht->size);
+	current = ht->array[index];
+/*Traverse and get key value*/
+	while (current != NULL)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+			return ht->array[index]->value;
+		current = current->next;
+	}
+/*Returns Null is not found*/
+	return (NULL);
+}
