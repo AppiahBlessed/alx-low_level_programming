817ed991948b0418c4f003caca549195e0ff0eac
AppiahBlessed <kwabenadicey@gmail.com>
2023-08-24 02:16:40 +0000

    Island perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    The fucntion calculates the perimeter of the island described in the grid.
+
+    :param grid: Grid made up of integers
+    :return: Retursn the perimeter of the island
+    """
+    perimeter = 0
+
+    for row in range(len(grid)):
+        for col in range(len(grid[0])):
+            if grid[row][col] == 1:
+                perimeter += 4
+
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
+
